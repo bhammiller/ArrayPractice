a5e8542aab4456303baa65091e53f5a629a34f05
Brandon Hammiller <bdayton1985@gmail.com>
2018-01-26 16:37:09 -0500

    Final Boredom Tweaks

--- src/main/java/com/example/demo/ArrayPractice.java
+++ src/main/java/com/example/demo/ArrayPractice.java
@@ -12,13 +12,27 @@ public class ArrayPractice {
         do{
             System.out.println("What is your favorite book or movie?");
             String favorite_answer = keyboard.nextLine();
+            while(favorite_answer.equals("")){
+                System.out.println("Please enter a book or a movie");
+                favorite_answer = keyboard.nextLine();
+            }
             System.out.println("Is this a book or movie?");
             String type_answer = keyboard.nextLine();
+            while(!(type_answer.equals("book")||type_answer.equals("Book")||type_answer.equals("movie")
+                    ||type_answer.equals("Movie"))) {
+                System.out.println("Is that a movie or a book?");
+                type_answer = keyboard.nextLine();
+            }
             System.out.println("Do you have any more favorite books or movies? (Y/N)");
             continue_answer = keyboard.nextLine();
-            if (type_answer.equals("book")){
+            while(!(continue_answer.equals("Y")||continue_answer.equals("N")||continue_answer.equals("y")
+                    ||continue_answer.equals("n"))){
+                System.out.println("Please enter Y or N");
+                continue_answer = keyboard.nextLine();
+            }
+            if (type_answer.equals("book")||type_answer.equals("Book")){
                 favorite_books.add(favorite_answer);
-            }else if(type_answer.equals("movie")){
+            }else {
                 favorite_movies.add(favorite_answer);
             }
         } while (continue_answer.equals("Y") || continue_answer.equals("y"));
